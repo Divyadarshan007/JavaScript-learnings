c662bb51b2065c3d9cbabeb964475dd3adf3e041
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-01-27 15:57:24 +0530

    javascript learnings

---
+++ lecture-1(introduction)/index.js
@@ -0,0 +1,9 @@
+// javascript is scripting language or web based programming language which is used to create dynamic and interactive websites.
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
